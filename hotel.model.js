8e248fdd71f69addff83c283ad9d9c8047f3eb48
imauice <102501547+imauice@users.noreply.github.com>
2023-02-23 07:01:18 +0700

    Create hotel.model.js

---
+++ hotel.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+// Define the schema for the Hotel entity
+const hotelSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  address: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String },
+  country: { type: String, required: true },
+  latitude: { type: Number, required: true },
+  longitude: { type: Number, required: true },
+  rating: { type: Number },
+  image: { type: String },
+  amenities: { type: [String] },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now }
+});
+
+// Define the schema for the Room entity
+const roomSchema = new mongoose.Schema({
+  number: { type: Number, required: true },
+  type: { type: String, required: true },
+  price: { type: Number, required: true },
+  status: { type: String, default: "available" },
+  hotel_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Hotel', required: true },
+  bookings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Booking' }]
+});
+
+// Define the schema for the Booking entity
+const bookingSchema = new mongoose.Schema({
+  check_in_date: { type: Date, required: true },
+  check_out_date: { type: Date, required: true },
+  room_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  total_price: { type: Number, required: true }
+});
+
+// Define the models for each entity using their respective schemas
+const Hotel = mongoose.model('Hotel', hotelSchema);
+const Room = mongoose.model('Room', roomSchema);
+const Booking = mongoose.model('Booking', bookingSchema);
